d3cad8403cbb4dc5dc3a427ad937ef5471f7483f
Harold Ozouf <harold.ozouf@gmail.com>
2023-03-21 16:30:06 +0100

    feat: api management

--- hack/lint.sh
+++ hack/lint.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-CT_ARGS=""
 GIT_SAFE_DIR="false"
 
 if [ "$GIT_SAFE_DIR" != "true" ]; then
     git config --global --add safe.directory /charts
 fi
 
-CT_ARGS="--charts ${PWD}/charts"
-
-ct lint --config=./.github/chart-testing.yaml
+ct lint --config=./.github/chart-testing.yaml --chart-dirs .
